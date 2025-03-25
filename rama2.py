5c2a73fbde108d7b8f320ed6b3ab54cf4e514df7
LucianoGuijarro <guijarro.luciano1995@gmail.com>
2025-03-24 22:10:14 -0300

    se crea nueva rama con un saludo mas completo

---
+++ rama2.py
@@ -0,0 +1,6 @@
+nombre = input("Ingresa tu nombre: ")
+apellido = input("Ingresa tu apeliido: ")
+edad = int(input(f"Por favor ingrese su edad {nombre} "))
+pais = input("Ingrese su pais en donde reside ")
+
+print(f"Hola {nombre} {apellido} de {edad} años y que vive en {pais}")
