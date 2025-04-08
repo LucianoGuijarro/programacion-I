6f4eca91392e8501410132e91219a05822086841
LucianoGuijarro <guijarro.luciano1995@gmail.com>
2025-04-08 21:50:03 +1200

    se crea archivo ejercicio08

---
+++ guijarroLucianoTp04EstructurasRepetitivass/ejercicio08.py
@@ -0,0 +1,27 @@
+#8) Escribe un programa que permita al usuario ingresar 100 números enteros. Luego, el
+#programa debe indicar cuántos de estos números son pares, cuántos son impares, cuántos son
+#negativos y cuántos son positivos. (Nota: para probar el programa puedes usar una cantidad
+#menor, pero debe estar preparado para procesar 100 números con un solo cambio).
+
+numPares = 0
+numImpares = 0
+numPositivos = 0
+numNegativos = 0
+
+for i in range(0, 10):
+    num = int(input("Por favor ingrese un numero: "))
+    if num % 2 == 0 and num > 0:
+        numPares += 1
+        numPositivos +=1
+    elif num % 2 != 0 and num > 0:
+        numImpares +=1
+        numPositivos +=1
+    elif num < 0 and num % 2 == 0:
+        numNegativos += 1
+        numPares += 1
+    elif num < 0 and num % 2 != 0:
+        numNegativos += 1
+        numImpares +=1
+
+
+print(f"En esos numeros hay:\n {numPares} numeros pares\n {numImpares} numeros impares\n {numNegativos} numeros negativos\n {numPositivos} numeros positivos")
