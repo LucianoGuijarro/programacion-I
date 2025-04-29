7e823257e5ff55a5e03c0f3d299a3c0610abca29
LucianoGuijarro <guijarro.luciano1995@gmail.com>
2025-04-29 21:17:14 +1200

    Se crea y realiza el ejercicio04 y se agrega la consigna al ejercicio03

--- guijarroLucianoTp05.1Listas/ejercicio03.py
+++ guijarroLucianoTp05.1Listas/ejercicio03.py
@@ -1,3 +1,7 @@
+#3) Crear una lista vacía, agregar tres palabras con append e imprimir la lista resultante por
+#pantalla. Pista: para crear una lista vacía debes colocar los corchetes sin nada en su interior. Por
+#ejemplo:
+
 lista = []
 
 for i in range(1, 3 + 1):
