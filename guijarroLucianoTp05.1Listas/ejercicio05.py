489842da12e85a5e44c035b51db922c3d364e495
LucianoGuijarro <guijarro.luciano1995@gmail.com>
2025-04-29 21:22:01 +1200

    Se crea y realiza el ejercicio05 y ejercicio06

---
+++ guijarroLucianoTp05.1Listas/ejercicio05.py
@@ -0,0 +1,7 @@
+#5) Analizar el siguiente programa y explicar con tus palabras qué es lo que realiza.
+
+numeros = [8, 15, 3, 22,7]
+numeros.remove(max(numeros))
+print(numeros)
+
+#Este programa para mi lo que hace es eliminar el elemento con mayor valor de la lista que en este caso es el numero 22.
